5d4dc5f37a8330ef2cac5128be686694c8913eb7
csk2q <143836825+csk2q@users.noreply.github.com>
2024-02-26 15:26:53 -0600

    Create C# project

---
+++ Ai1 Search Methods/Ai1 Search Methods/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ai1_Search_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, search methods!");
+        }
+    }
+}
