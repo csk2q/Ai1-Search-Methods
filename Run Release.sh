db1fbc14ad871131caa56a939100842266e0d908
csk2q <143836825+csk2q@users.noreply.github.com>
2024-03-09 15:22:39 -0600

    Add misc files

---
+++ Run Release.sh
@@ -0,0 +1,6 @@
+cd "/Users/mcbabac/Library/CloudStorage/OneDrive-Personal/School/Umkc/2024 Spring - CompSci 461 - Introduction to Artificial Intelligence/Ai1-Search-Methods/Ai1 Search Methods/Ai1 Search Methods"
+dotnet build -c Release
+
+cd /Users/mcbabac/Library/CloudStorage/OneDrive-Personal/School/Umkc/2024\ Spring\ -\ CompSci\ 461\ -\ Introduction\ to\ Artificial\ Intelligence/Ai1-Search-Methods/Ai1\ Search\ Methods/Ai1\ Search\ Methods/bin/Release/net8.0
+
+"./Ai1 Search Methods"
